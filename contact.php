79c812e851894d595b9eaf5bec5f875efb83126a
Michael Erameh <eramehmichael2000@gmail.com>
2025-11-26 02:13:46 +0100

    update

--- contact.php
+++ contact.php
@@ -2,38 +2,13 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+# from /home/petevpcc/moneylitafrica.com
+echo "PWD: $(pwd)"
+echo "---- find PHPMailer.php ----"
+find vendor -type f -iname PHPMailer.php -print -exec ls -l {} \;
+echo "---- find SMTP.php ----"
+find vendor -type f -iname SMTP.php -print -exec ls -l {} \;
+echo "---- find Exception.php ----"
+find vendor -type f -iname Exception.php -print -exec ls -l {} \;
 
-$base = __DIR__;
-$tried = [];
-
-$paths = [
-    $base . '/vendor/phpmailer/phpmailer/src/Exception.php',
-    $base . '/vendor/phpmailer/phpmailer/src/PHPMailer.php',
-    $base . '/vendor/phpmailer/phpmailer/src/SMTP.php',
-
-    $base . '/vendor/phpmailer/phpmailer/PHPMailer/src/Exception.php',
-    $base . '/vendor/phpmailer/phpmailer/PHPMailer/src/PHPMailer.php',
-    $base . '/vendor/phpmailer/phpmailer/PHPMailer/src/SMTP.php',
-
-    $base . '/vendor/phpmailer/phpmailer-master/src/Exception.php',
-    $base . '/vendor/phpmailer/phpmailer-master/src/PHPMailer.php',
-    $base . '/vendor/phpmailer/phpmailer-master/src/SMTP.php',
-];
-
-foreach ($paths as $p) {
-    if (file_exists($p)) {
-        require_once $p;
-        error_log("PHPMailer loader: required $p");
-    } else {
-        $tried[] = $p;
-    }
-}
-
-if (!class_exists('PHPMailer\PHPMailer\PHPMailer', false)) {
-    error_log("PHPMailer loader: class still missing after trying paths: " . implode(', ', $tried));
-    // You can optionally die with a friendly message while debugging:
-    // die('PHPMailer class could not be loaded - check logs.');
-}else{
-    echo "success";
-}
 ?>
